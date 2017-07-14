ed0ad685f2e7927224752c9c424aa2d435ba24f8
Pontus Persson <pontus.alexander@gmail.com>
2017-07-14 15:48:13 +0200

    Rename object to entity.

--- src/traits/Spawn.js
+++ src/traits/Spawn.js
@@ -2,19 +2,19 @@ const {Vector2} = require('three');
 const {Spawn} = require('@snakesilk/platform-traits');
 
 function factory(parser, node) {
-    const itemNodes = node.getElementsByTagName('item');
+    const entityNodes = node.getElementsByTagName('entity');
     const items = [];
 
-    for (let itemNode, i = 0; itemNode = itemNodes[i]; ++i) {
-        const offsetNode = itemNode.getElementsByTagName('offset')[0];
+    for (let entityNode, i = 0; entityNode = entityNodes[i]; ++i) {
+        const offsetNode = entityNode.getElementsByTagName('offset')[0];
         let offset;
         if (offsetNode) {
             offset = parser.getVector3(offsetNode) || undefined;
         }
-        const event = parser.getAttr(itemNode, 'event') || 'death';
-        const object = parser.getAttr(itemNode, 'object');
-        const constr = parser.loader.resourceManager.get('object', object);
-        items.push([event, constr, offset]);
+        const event = parser.getAttr(entityNode, 'event') || 'death';
+        const entityId = parser.getAttr(entityNode, 'id');
+        const constructor = parser.loader.resourceManager.get('entity', entityId);
+        items.push([event, constructor, offset]);
     }
 
     return function createSpawn() {
